5215869d34345be8b387a209802186c3e39a2a2d
Torbjörn Rathsman <milasudril@gmail.com>
2020-06-13 11:00:14 +0200

    Test and fix I/O

--- model/pixel.hpp
+++ model/pixel.hpp
@@ -8,88 +8,93 @@
 
 #include <ranges>
 #include <span>
-#include <vector>
+#include <memory>
 
 namespace Texpainter::Model
 {
 	class Pixel
 	{
 	public:
-		Pixel& red(float val)
+		constexpr explicit Pixel(float r, float g, float b, float a = 1.0f):
+			m_value{r,g,b,a}{}
+
+		constexpr Pixel() = default;
+
+		constexpr Pixel& red(float val)
 		{
 			return *this;
 		}
 
-		Pixel& green(float val)
+		constexpr Pixel& green(float val)
 		{
 			return *this;
 		}
 
-		Pixel& blue(float val)
+		constexpr Pixel& blue(float val)
 		{
 			return *this;
 		}
 
-		Pixel& alpha(float val)
+		constexpr Pixel& alpha(float val)
 		{
 			return *this;
 		}
 
 
 
-		float red() const
+		constexpr float red() const
 		{
 			return m_value[0];
 		}
 
-		float green() const
+		constexpr float green() const
 		{
 			return m_value[1];
 		}
 
-		float blue() const
+		constexpr float blue() const
 		{
 			return m_value[2];
 		}
 
-		float alpha() const
+		constexpr float alpha() const
 		{
 			return m_value[3];
 		}
 
 
 
-		Pixel& operator+=(Pixel other)
+		constexpr Pixel& operator+=(Pixel other)
 		{
 			m_value += other.m_value;
 			return *this;
 		}
 
-		Pixel& operator-=(Pixel other)
+		constexpr Pixel& operator-=(Pixel other)
 		{
 			m_value -= other.m_value;
 			return *this;
 		}
 
-		Pixel& operator/=(Pixel other)
+		constexpr Pixel& operator/=(Pixel other)
 		{
 			m_value /= other.m_value;
 			return *this;
 		}
 
-		Pixel& operator*=(Pixel other)
+		constexpr Pixel& operator*=(Pixel other)
 		{
 			m_value *= other.m_value;
 			return *this;
 		}
 
-		Pixel& operator*=(float factor)
+		constexpr Pixel& operator*=(float factor)
 		{
 			m_value *= factor;
 			return *this;
 		}
 
-		Pixel& operator/=(float factor)
+		constexpr Pixel& operator/=(float factor)
 		{
 			m_value /= factor;
 			return *this;
@@ -99,6 +104,32 @@ namespace Texpainter::Model
 		vec4_t m_value;
 	};
 
+	constexpr Pixel red()
+	{ return Pixel{1.0f, 0.0f, 0.0f};}
+
+	constexpr Pixel green()
+	{ return Pixel{0.0f, 1.0f, 0.0f};}
+
+	constexpr Pixel blue()
+	{ return Pixel{0.0f, 0.0f, 1.0f};}
+
+	constexpr Pixel black()
+	{ return Pixel{0.0f, 0.0f, 0.0f};}
+
+	constexpr Pixel white()
+	{ return Pixel{1.0f, 1.0f, 1.0f};}
+
+	constexpr bool operator==(Pixel a, Pixel b)
+	{
+		a-=b;
+		return a.red() == 0.0f && a.green() == 0.0f && a.blue() == 0.0f && a.alpha() == 0.0f;
+	}
+
+	constexpr bool operator!=(Pixel a, Pixel b)
+	{
+		return !(a == b);
+	}
+
 	template<std::ranges::contiguous_range Range, class OutputStream>
 	requires std::is_same_v<typename Range::value_type, Pixel>
 	void write(Range pixels, OutputStream stream)
@@ -107,11 +138,13 @@ namespace Texpainter::Model
 	}
 
 	template<class SizedArray, class InputStream>
-	SizedArray read(Empty<std::vector<Pixel>>, InputStream stream)
+	SizedArray read(Empty<SizedArray>, InputStream stream)
 	{
-		auto values = read(stream, Empty<float*>{});
-		auto ptr = reinterpret_cast<Pixel const*>(values.release());
-		return SizedArray{ptr, std::size(values)};
+		auto data = read(stream, Empty<float[]>{});
+		using DataBlock = std::decay_t<decltype(data)>;
+		auto const size =data.size()/4;
+		auto const ptr = reinterpret_cast<Pixel*>(data.release());
+		return SizedArray{typename DataBlock::pointer_wrapper<Pixel>{ptr}, size};
 	}
 }
 #endif
