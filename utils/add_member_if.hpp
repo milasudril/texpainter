2dc3bdd89184f2b150567302647d585d8dd1cbc7
Torbjörn Rathsman <milasudril@gmail.com>
2020-06-14 21:07:13 +0200

    Add member if meta-program

---
+++ utils/add_member_if.hpp
@@ -0,0 +1,40 @@
+//@	{"targets":[{"name":"add_member_if.hpp","type":"include"}]}
+
+#ifndef TEXPAINTER_ADDMEMBERIF_HPP
+#define TEXPAINTER_ADDMEMBERIF_HPP
+
+namespace Texpainter
+{
+	template<bool cond, class T, int tag>
+	class AddMemberIf
+	{
+	public:
+		void value(const T&)
+		{
+		}
+		T value() const noexcept
+		{
+			return T{};
+		}
+	};
+
+	template<class T, int tag>
+	struct AddMemberIf<true, T, tag>
+	{
+	public:
+		typedef T type;
+		const T& value() const noexcept
+		{
+			return m_value;
+		}
+		void value(const T& val)
+		{
+			m_value = val;
+		}
+
+	private:
+		type m_value;
+	};
+}
+
+#endif
