7d6be20d209ae1ad776dec8407f9d7f4b63ece89
Torbjörn Rathsman <milasudril@gmail.com>
2020-06-16 11:06:36 +0200

    Add label class

---
+++ ui/label.hpp
@@ -0,0 +1,51 @@
+//@	{
+//@  "targets":[{"name":"label.hpp","type":"include"}]
+//@	,"dependencies_extra":[{"ref":"label.o","rel":"implementation"}]
+//@	}
+
+#ifndef TEXPAINTER_UI_LABEL_HPP
+#define TEXPAINTER_UI_LABEL_HPP
+
+#include "./container.hpp"
+
+#include <utility>
+
+namespace Texpainter::Ui
+{
+	class Label
+	{
+	public:
+		explicit Label(Container& container, const char* content);
+		~Label();
+
+		Label& operator=(Label&& obj) noexcept
+		{
+			std::swap(obj.m_impl, m_impl);
+			return *this;
+		}
+
+		Label(Label&& obj) noexcept: m_impl(obj.m_impl)
+		{
+			obj.m_impl = nullptr;
+		}
+
+		Label& content(const char* text);
+
+		const char* content() const noexcept;
+
+		Label& small(bool status);
+
+		Label& alignment(float x);
+
+
+	protected:
+		class Impl;
+		Impl* m_impl;
+
+		explicit Label(Impl& impl): m_impl(&impl)
+		{
+		}
+	};
+}
+
+#endif
