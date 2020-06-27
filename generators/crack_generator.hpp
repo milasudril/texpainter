5520b8308a59a8bb065a0ad782d594cd98b071ef
Torbjörn Rathsman <milasudril@gmail.com>
2020-06-27 11:07:12 +0200

    Add support for thicker lines

--- generators/crack_generator.hpp
+++ generators/crack_generator.hpp
@@ -18,20 +18,23 @@ namespace Texpainter::Generators
 	namespace detail
 	{
 		template<class Rng, class Distribution>
-		void draw_line(Span2d<Model::Pixel> img, vec2_t from, vec2_t to, double, Rng&, Distribution&)
+		void draw_line(Span2d<Model::Pixel> img, vec2_t from, vec2_t to, double line_width, Rng&, Distribution&)
 		{
 			auto const dir = to - from;
 			auto const l = length(dir);
 			auto const v = dir/l;
-		//	TODO: auto const n = vec2_t{-v[1], v[0]};
+			auto const n = vec2_t{-v[1], v[0]};
 			for(int t = 0; t < static_cast<int>(l + 0.5); ++t)
 			{
-				auto const pos = from + static_cast<double>(t)*v;
-				auto pos_x = static_cast<int>(pos[0]);
-				auto pos_y = static_cast<int>(pos[1]);
-				pos_x = (pos_x < 0 ? pos_x + img.width() : pos_x)%img.width();
-				pos_y = (pos_y < 0 ? pos_y + img.height() : pos_y)%img.height();
-				img(pos_x, pos_y) = Model::Pixel{1.0, 1.0, 1.0, 1.0};
+				for(int s = -static_cast<int>(line_width); s < static_cast<int>(line_width + 0.5); ++s)
+				{
+					auto const pos = from + static_cast<double>(t)*v + static_cast<double>(s)*0.5*n;
+					auto pos_x = static_cast<int>(pos[0]);
+					auto pos_y = static_cast<int>(pos[1]);
+					pos_x = (pos_x < 0 ? pos_x + img.width() : pos_x)%img.width();
+					pos_y = (pos_y < 0 ? pos_y + img.height() : pos_y)%img.height();
+					img(pos_x, pos_y) = Model::Pixel{1.0, 1.0, 1.0, 1.0};
+				}
 			}
 		}
 
@@ -50,8 +53,8 @@ namespace Texpainter::Generators
 		using Rng = std::mt19937;
 
 		CrackGenerator():
-		   m_n_cracks{1},
-		   m_line_width{1.0f},
+		   m_n_cracks{2},
+		   m_line_width{1.0f/256.0f},
 		   m_seg_length{1.0/64.0},
 		   m_branch_prob{0.25},
 		   m_max_length{0.25}
@@ -64,8 +67,7 @@ namespace Texpainter::Generators
 			std::uniform_int_distribution<uint32_t> init_pos_x{0, output_size.width() - 1};
 			std::uniform_int_distribution<uint32_t> init_pos_y{0, output_size.height() - 1};
 			std::exponential_distribution<double> seg_length_dist{1.0/(m_seg_length * size)};
-			printf("%08x\n", Angle{-0.25, Angle::Turns{}}.bits());
-			printf("%08x\n", Angle{0.25, Angle::Turns{}}.bits());
+
 			std::uniform_int_distribution<int32_t> dir_dist{
 				static_cast<int32_t>(Angle{-0.1666, Angle::Turns{}}.bits()),
 				static_cast<int32_t>(Angle{0.1666, Angle::Turns{}}.bits())};
@@ -107,7 +109,7 @@ namespace Texpainter::Generators
 							auto x_next = line.x + seg_length*vec2_t{cos(line.dir), sin(line.dir)};
 							traveled_distance += seg_length;
 							detail::draw_line(ret.pixels(), line.x, x_next, line.width, m_rng, max_depth);
-							line.width *= 0.9;
+							line.width *= 0.95;
 							if(branch(m_rng))
 							{
 								line.x = x_next;
