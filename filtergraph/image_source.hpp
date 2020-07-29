1c6b27000a0f4d62cb10d28d06120ce56996b9b4
Torbjörn Rathsman <milasudril@gmail.com>
2020-07-29 20:30:33 +0200

    Implemenet ImageSource

--- filtergraph/image_source.hpp
+++ filtergraph/image_source.hpp
@@ -11,6 +11,35 @@ namespace Texpainter::FilterGraph
 	class ImageSource
 	{
 	public:
+		explicit ImageSource(Span2d<PixelType const> pixels):r_pixels{pixels}{}
+
+		std::span<std::string_view const> paramNames() const
+		{
+			return std::span<std::string_view const>{};
+		}
+
+		std::vector<ProcParamValue> paramValues() const { return std::vector<ProcParamValue>{}; }
+
+		void set(std::string_view, ProcParamValue) {}
+
+		ProcParamValue get(std::string_view) const { return ProcParamValue{0.0}; }
+
+		std::span<std::string_view const> outputPorts() const
+		{
+			constexpr std::string_view ret[] = {"Input"};
+			return std::span<std::string_view const>{ret, 1};
+		}
+
+		std::span<std::string_view const> inputPorts() const
+		{
+			return std::span<std::string_view const>{};
+		}
+
+		std::vector<ProcResultType> operator()(std::span<ProcArgumentType const>) const
+		{
+			return std::vector<ProcResultType>{Model::BasicImage<PixelType>{r_pixels}};
+		}
+
 	private:
 		Span2d<PixelType const> r_pixels;
 	};
