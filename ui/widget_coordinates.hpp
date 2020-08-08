6abace7dd0cc916155a24d05d1fc1cd7861fb1ac
Torbjörn Rathsman <mialsudril@gmail.com>
2020-08-08 08:03:28 +0200

    Add WidgetCoordinates class

---
+++ ui/widget_coordinates.hpp
@@ -0,0 +1,20 @@
+//@	{"targets":[{"name":"widget_coordinates.hpp", "type":"include"}]}
+
+#ifndef TEXPAINTER_UI_WIDGETCOORDINATES_HPP
+#define TEXPAINTER_UI_WIDGETCOORDINATES_HPP
+
+#include "utils/vec_t.hpp"
+
+namespace Texpainter::Ui
+{
+	class WidgetCoordinates
+	{
+	public:
+		constexpr explicit WidgetCoordinates(double x, double y):m_value{x, y}{}
+		
+	private:
+		vec2_t m_value;
+	};
+}
+
+#endif
