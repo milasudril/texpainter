ea971b69a1965addcd6a2196f633f730ea37e420
Torbjörn Rathsman <milasudril@gmail.com>
2020-08-14 20:45:55 +0200

    Add ImgProcArg class

---
+++ filtergraph/img_proc_arg.cpp
@@ -0,0 +1,10 @@
+//@	{
+//@	 "targets":[{"name":"img_proc_arg.test", "type":"application", "autorun":1}]
+//@	}
+
+#include "./img_proc_arg.hpp"
+
+int main()
+{
+
+}
---
+++ filtergraph/img_proc_arg.hpp
@@ -0,0 +1,55 @@
+//@	{
+//@	 "targets":[{"name":"img_proc_arg.hpp", "type":"include"}]
+//@	}
+
+#ifndef TEXPAINTER_FILTERGRAPH_IMG_PROC_ARG_HPP
+#define TEXPAINTER_FILTERGRAPH_IMG_PROC_ARG_HPP
+
+#include "./argtuple.hpp"
+#include "./node_argument.hpp"
+#include "./proctypes.hpp"
+
+#include "utils/size_2d.hpp"
+
+#include <array>
+#include <functional>
+
+namespace Texpainter::FilterGraph
+{
+	namespace detail
+	{
+		template<ImageProcessor proc>
+		constexpr auto input_types()
+		{
+			std::array<PixelType, proc::inputs().size()> ret{};
+			std::ranges::transform(
+			    proc::inputs().inputs(), ret, [](auto val) { return val.type; });
+			return ret;
+		}
+	}
+
+	template<ImageProcessor ImgProc>
+	class ImgProcArg2
+	{
+	public:
+		explicit ImgProcArg2(NodeArgument const& node_args)
+		    : m_size{node_args.size()}
+		    , m_inputs{node_args.inputs<std::decay_t<decltype(m_inputs)>>()}
+		{
+		}
+
+		Size2d size() const { return m_size; }
+
+		template<size_t index>
+		auto input(uint32_t col, uint32_t row) const
+		{
+			return m_inputs.template get<index>()[row * m_size.width() + col];
+		}
+
+	private:
+		Size2d m_size;
+		ArgTuple<detail::input_types<ImgProc>()> m_inputs;
+	};
+}
+
+#endif
