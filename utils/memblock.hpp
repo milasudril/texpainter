f2fcc329c78bf5ed711ccd0be2b7d53ac2b72d17
Torbjörn Rathsman <milasudril@gmail.com>
2020-08-15 17:11:56 +0200

    Add malloc/free wrapper

---
+++ utils/memblock.hpp
@@ -0,0 +1,50 @@
+//@	{
+//@	 "targets":[{"name":"memblock.hpp", "type":"include"}]
+//@	}
+
+#ifndef TEXPAINTER_MEMBLOCK_HPP
+#define TEXPAINTER_MEMBLOCK_HPP
+
+#include <cstdlib>
+
+namespace Texpainter
+{
+	class Memblock
+	{
+		public:
+			explicit Memblock(size_t size): m_block{malloc(size)}
+			{}
+
+			~Memblock()
+			{
+				if(m_block != nullptr)
+				{ free(m_block); }
+			}
+
+			Memblock(Memblock&& other) noexcept: m_block{ other.release() }
+			{}
+
+			Memblock& operator=(Memblock&& other) noexcept
+			{
+				free(m_block);
+				m_block = other.release();
+				return *this;
+			}
+
+			void* get() const
+			{return m_block;}
+
+			void* release()
+			{
+				auto ret = m_block;
+				m_blocK = nullptr;
+				return m_block;
+			}
+
+		private:
+			void* m_block;
+	};
+
+}
+
+#endif
