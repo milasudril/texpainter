c482541855cd10e0d127f11b3fd641972c8a505f
Torbjörn Rathsman <milasudril@gmail.com>
2020-08-16 08:11:48 +0200

    ImageProcessorWrapper implements AbstractImageProcessor

---
+++ filtergraph/abstract_image_processor.hpp
@@ -0,0 +1,33 @@
+//@	{
+//@	 "targets":[{"name":"abstract_image_processor.hpp", "type":"include"}]
+//@	}
+
+#ifndef TEXPAINTER_FILTERGRAPH_ABSTRACTIMAGEPROCESSOR_HPP
+#define TEXPAINTER_FILTERGRAPH_ABSTRACTIMAGEPROCESSOR_HPP
+
+#include "./node_argument.hpp"
+#include "./proctypes.hpp"
+
+#include "utils/memblock.hpp"
+
+namespace Texpainter::FilterGraph
+{
+	class AbstractImageProcessor
+	{
+	public:
+		static constexpr size_t MaxNumOutputs = 4;
+
+		virtual std::array<Memblock, MaxNumOutputs> operator()(NodeArgument const& arg) const = 0;
+		virtual std::span<PortInfo const> inputPorts() const                                  = 0;
+		virtual std::span<PortInfo const> outputPorts() const                                 = 0;
+		virtual std::span<char const* const> paramNames() const                               = 0;
+		virtual std::span<ParamValue const> paramValues() const                               = 0;
+		virtual ParamValue get(std::string_view param_name) const                             = 0;
+		virtual AbstractImageProcessor& set(std::string_view param_name, ParamValue value)    = 0;
+		virtual std::unique_ptr<AbstractImageProcessor> clone() const                         = 0;
+		virtual char const* name() const                                                      = 0;
+		virtual ~AbstractImageProcessor() = default;
+	};
+}
+
+#endif
--- filtergraph/image_processor_wrapper.hpp
+++ filtergraph/image_processor_wrapper.hpp
@@ -8,6 +8,7 @@
 #include "./node_argument.hpp"
 #include "./proctypes.hpp"
 #include "./img_proc_arg.hpp"
+#include "./abstract_image_processor.hpp"
 
 #include "utils/memblock.hpp"
 
@@ -45,12 +46,12 @@ namespace Texpainter::FilterGraph
 	}
 
 	template<ImageProcessor Proc>
-	class ImageProcessorWrapper
+	class ImageProcessorWrapper: public AbstractImageProcessor
 	{
 	public:
 		explicit ImageProcessorWrapper(Proc&& proc): m_proc{std::move(proc)} {}
 
-		std::array<Memblock, 4> operator()(NodeArgument const& arg) const
+		std::array<Memblock, 4> operator()(NodeArgument const& arg) const override
 		{
 			auto ret = detail::alloc_output_buffers<Proc>(arg.size());
 
@@ -68,6 +69,32 @@ namespace Texpainter::FilterGraph
 
 			return ret;
 		}
+
+		std::span<PortInfo const> inputPorts() const override { return m_proc.inputPorts(); }
+
+		std::span<PortInfo const> outputPorts() const override { return m_proc.outputPorts(); }
+
+		std::span<char const* const> paramNames() const override { return m_proc.paramNames(); }
+
+		std::span<ParamValue const> paramValues() const override { return m_proc.paramValues(); }
+
+		ParamValue get(std::string_view param_name) const override
+		{
+			return m_proc.get(param_name);
+		}
+
+		AbstractImageProcessor& set(std::string_view param_name, ParamValue value) override
+		{
+			m_proc.set(param_name, value);
+			return *this;
+		}
+
+		std::unique_ptr<AbstractImageProcessor> clone() const override
+		{
+			return std::make_unique<ImageProcessorWrapper>(*this);
+		}
+
+		char const* name() const override { return Proc::name(); }
 
 	private:
 		Proc m_proc;
