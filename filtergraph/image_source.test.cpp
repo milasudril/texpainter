ab5e7ad3baa06bd34d985c777af1755b3040ff65
Torbjörn Rathsman <milasudril@gmail.com>
2020-08-20 18:06:41 +0200

    Check that name is not nullptr

--- filtergraph/image_source.test.cpp
+++ filtergraph/image_source.test.cpp
@@ -45,11 +45,13 @@ namespace Testcases
 	void texpainterFilterGraphImageSourceCall()
 	{
 		std::array<Texpainter::FilterGraph::RealValue, 6> pixels{1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
-		auto size =Texpainter::Size2d{3, 2};
-		Texpainter::FilterGraph::ImageSource<Texpainter::FilterGraph::RealValue> src{Texpainter::Span2d{pixels.data(), size}};
-		using InterfaceDescriptor = Texpainter::FilterGraph::ImageSource<Texpainter::FilterGraph::RealValue>::InterfaceDescriptor;
+		auto size = Texpainter::Size2d{3, 2};
+		Texpainter::FilterGraph::ImageSource<Texpainter::FilterGraph::RealValue> src{
+		    Texpainter::Span2d{pixels.data(), size}};
+		using InterfaceDescriptor = Texpainter::FilterGraph::ImageSource<
+		    Texpainter::FilterGraph::RealValue>::InterfaceDescriptor;
 		using ImgProcArg = Texpainter::FilterGraph::ImgProcArg2<InterfaceDescriptor>;
-		using InputArgs = ImgProcArg::InputArgs;
+		using InputArgs  = ImgProcArg::InputArgs;
 		using OutputArgs = ImgProcArg::OutputArgs;
 
 		std::array<Texpainter::FilterGraph::RealValue, 6> pixels_out{};
@@ -58,53 +60,14 @@ namespace Testcases
 		src(ImgProcArg{size, InputArgs{}, args});
 		assert(std::ranges::equal(pixels, pixels_out));
 	}
+
+	void texpainterFilterGraphImageSourceName()
+	{
+		static_assert(
+		    Texpainter::FilterGraph::ImageSource<Texpainter::FilterGraph::RealValue>::name()
+		    != nullptr);
+	}
 }
-
-#if 0
-
-
-		void operator()(ImgProcArg2<InterfaceDescriptor> const& args) const
-		{
-			std::ranges::copy(r_pixels, args.template output<0>());
-		}
-
-		ParamValue get(ParamName) const { return ParamValue{0.0}; }
-
-		void set(ParamName, ParamValue) {}
-
-
-		explicit ImageSource(Span2d<PixelType const> pixels): r_pixels{pixels} {}
-
-		static constexpr std::span<char const* const> paramNames()
-		{
-			return std::span<char const* const>{};
-		}
-
-		std::span<ParamValue const> paramValues() const { return std::span<ParamValue const>{}; }
-
-		void set(std::string_view, ParamValue) {}
-
-		ParamValue get(std::string_view) const { return ParamValue{0.0}; }
-
-		static constexpr std::span<PortInfo const> outputPorts()
-		{
-			return std::span<PortInfo const>{s_output_ports};
-		}
-
-		static constexpr std::span<PortInfo const> inputPorts()
-		{
-			return std::span<PortInfo const>{};
-		}
-
-		std::vector<ImgProcRetval> operator()(std::span<ImgProcArg const>) const
-		{
-			return std::vector<ImgProcRetval>{PixelStore::BasicImage<PixelType>{r_pixels}};
-		}
-
-
-		static constexpr char const* name() { return "Layer input"; }
-
-#endif
 
 int main()
 {
