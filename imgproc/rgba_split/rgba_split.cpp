59d0fb6cdae882e468986ad67602a699195687fe
Torbjörn Rathsman <milasudril@gmail.com>
2020-08-21 17:26:26 +0200

    Add implementation for new call operator

--- imgproc/rgba_split/rgba_split.cpp
+++ imgproc/rgba_split/rgba_split.cpp
@@ -33,3 +33,18 @@ std::vector<RgbaSplit::RetVal> RgbaSplit::ImageProcessor::operator()(
 	}
 	return std::vector<RgbaSplit::RetVal>{};
 }
+
+void RgbaSplit::ImageProcessor::operator()(ImgProcArg<InterfaceDescriptor> const& arg) const
+{
+	auto size = arg.size();
+	for(uint32_t row = 0; row < size.height(); ++row)
+	{
+		for(uint32_t col = 0; col < size.width(); ++col)
+		{
+			arg.output<0>(col, row) = arg.input<0>(col, row).red();
+			arg.output<1>(col, row) = arg.input<0>(col, row).green();
+			arg.output<2>(col, row) = arg.input<0>(col, row).blue();
+			arg.output<3>(col, row) = arg.input<0>(col, row).alpha();
+		}
+	}
+}
