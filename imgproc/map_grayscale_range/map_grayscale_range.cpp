e5b7abe6cbe76556110f5b19dfbae217d3b1b2dd
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-01 13:38:15 +0100

    Output range params for map grayscale range

--- imgproc/map_grayscale_range/map_grayscale_range.cpp
+++ imgproc/map_grayscale_range/map_grayscale_range.cpp
@@ -10,11 +10,14 @@ void MapGrayscaleRange::ImageProcessor::operator()(
     ImgProcArg<InterfaceDescriptor> const& args) const
 {
 	auto const size = args.size().area();
-
 	std::transform(args.input<0>(),
 	               args.input<0>() + size,
 	               args.output<0>(),
-	               [min_in = args.input<1>(), max_in = args.input<2>()](auto val) {
-		               return (val - min_in) / (max_in - min_in);
+	               [min_in  = args.input<1>(),
+	                max_in  = args.input<2>(),
+	                min_out = static_cast<float>(m_params.find<Str{"Min"}>()->value()),
+	                max_out = static_cast<float>(m_params.find<Str{"Max"}>()->value())](auto val) {
+		               auto const t = (val - min_in) / (max_in - min_in);
+		               return t * max_out + (1.0f - t) * min_out;
 	               });
 }
