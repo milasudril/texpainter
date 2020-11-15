82b2f493891091f7715e47a5641d6b3ba1252f37
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-15 09:11:30 +0100

    Add mutex to fftw planner interface

--- dft/plan.cpp
+++ dft/plan.cpp
@@ -5,6 +5,13 @@
 #include "./plan.hpp"
 #include "utils/datablock.hpp"
 
+#include <mutex>
+
+namespace
+{
+	constinit std::mutex FftwPlanMutex;
+}
+
 Texpainter::Dft::BasicPlan::BasicPlan(Size2d size, Direction dir)
 {
 	auto const n     = size.area();
@@ -13,11 +20,12 @@ Texpainter::Dft::BasicPlan::BasicPlan(Size2d size, Direction dir)
 	std::fill(input_buff.get(), input_buff.get() + n, 0);
 	auto input_buff_ptr  = reinterpret_cast<fftw_complex*>(input_buff.get());
 	auto output_buff_ptr = reinterpret_cast<fftw_complex*>(output_buff.get());
-	m_plan               = std::unique_ptr<PlanType, PlanDeleter>{fftw_plan_dft_2d(size.height(),
-                                                                     size.width(),
-                                                                     input_buff_ptr,
-                                                                     output_buff_ptr,
-                                                                     static_cast<int>(dir),
-                                                                     FFTW_MEASURE),
-                                                    PlanDeleter{}};
+	std::lock_guard fftw_plan_lock{FftwPlanMutex};
+	m_plan = std::unique_ptr<PlanType, PlanDeleter>{fftw_plan_dft_2d(size.height(),
+	                                                                 size.width(),
+	                                                                 input_buff_ptr,
+	                                                                 output_buff_ptr,
+	                                                                 static_cast<int>(dir),
+	                                                                 FFTW_MEASURE),
+	                                                PlanDeleter{}};
 }
