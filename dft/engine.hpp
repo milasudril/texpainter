166ebcd218430620c1450e02636d4c48a8e4fb3d
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-15 09:25:55 +0100

    Minor refactor

--- dft/engine.hpp
+++ dft/engine.hpp
@@ -22,24 +22,12 @@ namespace Texpainter::Dft
 		         BasicPlan::sample_type const* input_buffer,
 		         BasicPlan::sample_type* output_buffer) const
 		{
-			auto i = std::ranges::find_if(m_plans,
-			                              [size](auto const& val) { return val.m_size == size; });
-
-			if(i == std::end(m_plans)) [[unlikely]]
-				{
-					i         = std::ranges::min_element(m_plans, [](auto const& a, auto const& b) {
-                        return a.m_last_used < b.m_last_used;
-                    });
-					i->m_size = size;
-					i->m_plan_fwd  = Plan<Direction::Forward>{size};
-					i->m_plan_bkwd = Plan<Direction::Backward>{size};
-				}
-			i->m_last_used = std::chrono::steady_clock::now();
+			auto& plan = get_plan(size);
 			if constexpr(dir == Direction::Forward)
-			{ i->m_plan_fwd.execute(input_buffer, output_buffer); }
+			{ plan.m_plan_fwd.execute(input_buffer, output_buffer); }
 			else
 			{
-				i->m_plan_bkwd.execute(input_buffer, output_buffer);
+				plan.m_plan_bkwd.execute(input_buffer, output_buffer);
 			}
 		}
 
@@ -53,6 +41,24 @@ namespace Texpainter::Dft
 		};
 
 		mutable std::array<PlanData, 16> m_plans;
+
+		PlanData const& get_plan(Size2d size) const
+		{
+			auto i = std::ranges::find_if(m_plans,
+			                              [size](auto const& val) { return val.m_size == size; });
+			if(i == std::end(m_plans)) [[unlikely]]
+				{
+					i         = std::ranges::min_element(m_plans, [](auto const& a, auto const& b) {
+                        return a.m_last_used < b.m_last_used;
+                    });
+					i->m_size = size;
+					i->m_plan_fwd  = Plan<Direction::Forward>{size};
+					i->m_plan_bkwd = Plan<Direction::Backward>{size};
+				}
+			i->m_last_used = std::chrono::steady_clock::now();
+
+			return *i;
+		}
 	};
 
 	Engine const& engineInstance();
