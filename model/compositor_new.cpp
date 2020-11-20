21c3151ebaf1654157db28f4adc06553c253ad7e
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-20 21:01:49 +0100

    Add new version of compositor

---
+++ model/compositor_new.cpp
@@ -0,0 +1,90 @@
+//@	{
+//@	  "targets":[{"name":"compositor_new.o", "type":"object"}]
+//@	}
+
+#include "./compositor_new.hpp"
+
+#include "utils/graphutils.hpp"
+
+Texpainter::FilterGraph::ValidationResult Texpainter::Model::validate(CompositorNew const& g)
+{
+	using FilterGraph::ValidationResult;
+
+	ValidationResult result{ValidationResult::NoError};
+	processGraphNodeRecursive(
+	    [&result]<class Tag>(auto const& node, Tag) {
+		    if constexpr(Tag::value == GraphProcessingEvent::LoopDetected)
+		    {
+			    result = ValidationResult::CyclicConnections;
+			    return GraphProcessing::Stop;
+		    }
+		    else if constexpr(Tag::value == GraphProcessingEvent::ProcessNode)
+		    {
+			    if(!isConnected(node))
+			    {
+				    result = ValidationResult::InputsNotConnected;
+				    return GraphProcessing::Stop;
+			    }
+			    return GraphProcessing::Continue;
+		    }
+	    },
+	    *g.node(CompositorNew::OutputNodeId));
+	return result;
+}
+
+void Texpainter::Model::CompositorNew::process(Span2d<PixelStore::Pixel> canvas) const
+{
+	assert(valid());
+	if(m_node_array.size() == 0) [[unlikely]]
+		{
+			std::vector<NodeState> nodes;
+			nodes.reserve(m_graph.size());
+			processGraphNodeRecursive(
+			    [&nodes](auto const& node, auto) {
+				    nodes.push_back(NodeState{std::cref(node),
+				                              std::make_unique<Sched::SignalingCounter<size_t>>()});
+
+				    // NOTE: This works because we are visiting nodes in topological order. Thus, it is
+				    //       known that all sources have been added to nodes.
+				    auto& item = nodes.back();
+				    std::ranges::for_each(
+				        item.node.get().inputs(),
+				        [&nodes, counter = item.counter.get()](auto& ref) {
+					        auto i = std::ranges::find_if(
+					            nodes, [&value = ref.processor()](auto const& item) {
+						            return &item.node.get() == &value;
+					            });
+					        assert(i != std::end(nodes));
+					        i->signal_counters[ref.port().value()].push_back(counter);
+				        });
+
+				    return GraphProcessing::Continue;
+			    },
+			    *r_output_node);
+			m_node_array = std::move(nodes);
+		}
+
+	r_output->sink(canvas);
+
+	// NOTE: Since OutputNode does not use the internal image cache (it has no outputs)
+	//       and it may happen that it is not connected to the input node, we must always
+	//       recompute the output node. Otherwise, the contents of ret will be undefined,
+	//       in case we already have computed the output result.
+	r_output_node->forceUpdate();
+
+	Sched::SignalingCounter<size_t> task_counter;
+
+	auto schedule_task = [&workers = m_workers, size = canvas.size(), &task_counter](auto& item) {
+		workers.addTask([&item, size, &task_counter]() {
+			item.counter->waitAndReset(item.node.get().inputPorts().size());
+			item.node(size);
+			std::ranges::for_each(item.signal_counters, [](auto& counter) {
+				std::ranges::for_each(counter, [](auto value) { ++(*value); });
+			});
+			++task_counter;
+		});
+	};
+
+	std::ranges::for_each(m_node_array, schedule_task);
+	task_counter.waitAndReset(m_node_array.size());
+}
---
+++ model/compositor_new.hpp
@@ -0,0 +1,162 @@
+//@	{
+//@	  "targets":[{"name":"compositor_new.hpp", "type":"include"}]
+//@	 ,"dependencies_extra":[{"ref":"compositor_new.o","rel":"implementation"}]
+//@	}
+
+#ifndef TEXPAINTER_MODEL_COMPOSITORNEW_HPP
+#define TEXPAINTER_MODEL_COMPOSITORNEW_HPP
+
+#include "filtergraph/graph.hpp"
+#include "filtergraph/layer_input.hpp"
+#include "filtergraph/image_sink.hpp"
+#include "pixel_store/image.hpp"
+#include "utils/iter_pair.hpp"
+#include "utils/pair_iterator.hpp"
+#include "sched/signaling_counter.hpp"
+#include "sched/thread_pool.hpp"
+
+#include <algorithm>
+
+namespace Texpainter::Model
+{
+	class CompositorNew;
+
+	Texpainter::FilterGraph::ValidationResult validate(CompositorNew const& g);
+
+	class CompositorNew
+	{
+	public:
+		static constexpr FilterGraph::NodeId InputNodeId{0};
+		static constexpr FilterGraph::NodeId OutputNodeId{1};
+		using NodeItem        = FilterGraph::Graph::NodeItem;
+		using NodeId          = FilterGraph::NodeId;
+		using InputPortIndex  = FilterGraph::InputPortIndex;
+		using OutputPortIndex = FilterGraph::OutputPortIndex;
+
+		CompositorNew(): m_valid_state{ValidationState::NotValidated}
+		{
+			using FilterGraph::ImageProcessorWrapper;
+			using FilterGraph::ImageSink;
+			using FilterGraph::LayerInput;
+
+			auto input    = std::make_unique<ImageProcessorWrapper<LayerInput>>(LayerInput{});
+			auto output   = std::make_unique<ImageProcessorWrapper<ImageSink>>(ImageSink{});
+			r_input       = &input->processor();
+			r_output      = &output->processor();
+			r_input_node  = &m_graph.insert(std::move(input)).second.get();
+			r_output_node = &m_graph.insert(std::move(output)).second.get();
+
+			connect(OutputNodeId,
+			        FilterGraph::InputPortIndex{0},
+			        InputNodeId,
+			        FilterGraph::OutputPortIndex{0});
+		}
+
+		CompositorNew(CompositorNew const& other): m_graph{other.m_graph}
+		{
+			using FilterGraph::ImageProcessorWrapper;
+			using FilterGraph::ImageSink;
+			using FilterGraph::LayerInput;
+
+			r_input_node  = m_graph.node(InputNodeId);
+			r_output_node = m_graph.node(OutputNodeId);
+			r_input = &dynamic_cast<ImageProcessorWrapper<LayerInput>*>(&r_input_node->processor())
+			               ->processor();
+			r_output = &dynamic_cast<ImageProcessorWrapper<ImageSink>*>(&r_output_node->processor())
+			                ->processor();
+		}
+
+		CompositorNew(CompositorNew&&) = default;
+		CompositorNew& operator=(CompositorNew&&) = default;
+
+		void process(Span2d<PixelStore::Pixel> canvas) const;
+
+		NodeItem insert(std::unique_ptr<FilterGraph::AbstractImageProcessor> proc)
+		{
+			return m_graph.insert(std::move(proc));
+		}
+
+		template<FilterGraph::ImageProcessor ImgProc>
+		NodeItem insert(ImgProc&& proc)
+		{
+			return m_graph.insert(std::forward<ImgProc>(proc));
+		}
+
+		CompositorNew& connect(NodeId a, InputPortIndex sink, NodeId b, OutputPortIndex src)
+		{
+			m_graph.connect(a, sink, b, src);
+			m_valid_state = ValidationState::NotValidated;
+			return *this;
+		}
+
+		CompositorNew& disconnect(NodeId a, InputPortIndex sink)
+		{
+			m_graph.disconnect(a, sink);
+			m_valid_state = ValidationState::NotValidated;
+			return *this;
+		}
+
+		auto node(FilterGraph::NodeId id) const { return m_graph.node(id); }
+
+		CompositorNew& erase(FilterGraph::NodeId id)
+		{
+			m_graph.erase(id);
+			m_valid_state = ValidationState::NotValidated;
+			return *this;
+		}
+
+		auto nodesWithId() const { return m_graph.nodesWithId(); }
+
+		auto nodesWithId() { return m_graph.nodesWithId(); }
+
+		bool valid() const
+		{
+			using FilterGraph::ValidationResult;
+
+			if(m_valid_state == ValidationState::NotValidated) [[unlikely]]
+				{
+					m_valid_state = validate(*this) == ValidationResult::NoError
+					                    ? ValidationState::ValidatedValid
+					                    : ValidationState::ValidatedNotValid;
+				}
+			return m_valid_state == ValidationState::ValidatedValid;
+		}
+
+		void clearValidationState()
+		{
+			m_valid_state = ValidationState::NotValidated;
+			m_node_array.clear();
+		}
+
+	private:
+		struct NodeState
+		{
+			using Node = FilterGraph::Node;
+			std::reference_wrapper<Node const> node;
+			std::unique_ptr<Sched::SignalingCounter<size_t>> counter;
+			std::array<std::vector<Sched::SignalingCounter<size_t>*>, Node::MaxNumOutputs>
+			    signal_counters{};
+		};
+		mutable std::vector<NodeState> m_node_array;
+		mutable Texpainter::Sched::ThreadPool m_workers;
+
+		enum class ValidationState : size_t
+		{
+			NotValidated,
+			ValidatedNotValid,
+			ValidatedValid
+		};
+		mutable ValidationState m_valid_state;
+
+		FilterGraph::LayerInput* r_input;
+		FilterGraph::ImageSink* r_output;
+		FilterGraph::Node* r_input_node;
+		FilterGraph::Node* r_output_node;
+
+		FilterGraph::Graph m_graph;
+	};
+
+	Texpainter::FilterGraph::ValidationResult validate(CompositorNew const& g);
+}
+
+#endif
