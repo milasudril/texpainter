d0e797710194542cf24ed40c981d26ab7352b71e
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-22 09:28:29 +0100

    Create input manager

--- model_new/document.hpp
+++ model_new/document.hpp
@@ -37,6 +37,68 @@ namespace Texpainter::Model
 		    processor;
 	};
 
+	template<class T>
+	class CompositorInputManager
+	{
+	public:
+		auto const& get(std::type_identity<T>) const { return m_inputs; }
+
+		auto const get(std::type_identity<T>, ItemName const& item) const
+		{
+			auto i = m_inputs.find(item);
+			return i != std::end(m_inputs) ? &i->second : nullptr;
+		}
+
+		auto insert(ItemName const& name,
+		            T&& img,
+		            Compositor& compositor,
+		            std::map<ItemName, Compositor::NodeItem>& nodes)
+		{
+			auto i = nodes.find(name);
+			if(i == std::end(nodes)) [[unlikely]]
+				{
+					return static_cast<CompositorInput<WithStatus<T>>*>(nullptr);
+				}
+
+			auto item = compositor.insert(T{std::string{toString(name)}});
+			nodes.insert(i, std::pair{name, item});
+
+			auto& node =
+			    dynamic_cast<FilterGraph::ImageProcessorWrapper<T>>(item.second.get().processor());
+			return &m_inputs
+			            .insert(std::pair{name, CompositorInput{WithStatus{std::move(img)}, node}})
+			            .first->second;
+		}
+
+		bool erase(std::type_identity<T>,
+		           ItemName const& item,
+		           Compositor& compositor,
+		           std::map<ItemName, Compositor::NodeItem>& nodes)
+		{
+			auto i = nodes.find(item);
+			if(i == std::end(nodes)) [[unlikely]] {return false;}
+
+			compositor.erase(i->second.first);
+			nodes.erase(i);
+			m_inputs.erase(item);
+		}
+
+		template<InplaceMutator<T> Mutator>
+		bool modify(Mutator&& mut, ItemName const& item)
+		{
+			auto i = m_inputs.find(item);
+			if(i == std::end(m_inputs)) [[unlikely]]
+				{
+					return false;
+				}
+
+			return i->second.source.modify(std::forward<Mutator>(mut));
+		}
+
+	private:
+		std::map<ItemName, CompositorInput<WithStatus<T>>> m_inputs;
+	};
+
 	class Document
 	{
 	public:
