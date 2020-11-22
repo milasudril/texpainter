36b5af1258c0c0a495855584cbd7a7941839710c
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-22 13:35:22 +0100

    Set force update in separate function

--- model_new/document.cpp
+++ model_new/document.cpp
@@ -15,29 +15,32 @@ bool Texpainter::Model::Document::dirty() const
 	                              [](auto const& item) { return item.second.source.dirty(); });
 }
 
+namespace
+{
+	void forceUpdateIfDirty(auto const& item, Texpainter::Model::Document const& doc)
+	{
+		if(item.second.source.dirty())
+		{
+			auto node_item = doc.inputNodeItem(item.first);
+			assert(node_item != nullptr);
+			node_item->second.get().forceUpdate();
+		}
+	}
+}
+
 Texpainter::PixelStore::Image Texpainter::Model::render(Document const& document)
 {
-	PixelStore::Image ret{document.canvasSize()};
-	std::ranges::for_each(document.images(), [&document](auto& item) {
+	std::ranges::for_each(document.images(), [&document](auto const& item) {
 		item.second.processor.get().processor().source(item.second.source.get().pixels());
-		if(item.second.source.dirty())
-		{
-			auto node_item = document.inputNodeItem(item.first);
-			assert(node_item != nullptr);
-			node_item->second.get().forceUpdate();
-		}
+		forceUpdateIfDirty(item, document);
 	});
 
-	std::ranges::for_each(document.palettes(), [&document](auto& item) {
+	std::ranges::for_each(document.palettes(), [&document](auto const& item) {
 		item.second.processor.get().processor().source(item.second.source.get());
-		if(item.second.source.dirty())
-		{
-			auto node_item = document.inputNodeItem(item.first);
-			assert(node_item != nullptr);
-			node_item->second.get().forceUpdate();
-		}
+		forceUpdateIfDirty(item, document);
 	});
 
+	PixelStore::Image ret{document.canvasSize()};
 	document.compositor().process(ret.pixels());
 	return ret;
 }
