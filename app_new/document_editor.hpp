bf682aae4024f687f469dd397d213cb2a3c469a9
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-26 19:42:13 +0100

    Create FilterGraphEditor

--- app_new/document_editor.hpp
+++ app_new/document_editor.hpp
@@ -8,16 +8,40 @@
 
 #include "model_new/document.hpp"
 
+#include "ui/window.hpp"
+
 namespace Texpainter::App
 {
-	class DocumentEditor
+	template<class Widget>
+	class WidgetWithWithWindow
 	{
 	public:
-		DocumentEditor(): m_document{Size2d{512, 512}} {}
+		template<class... Args>
+		explicit WidgetWithWithWindow(char const* title, Args&&... args)
+		    : m_window{title}
+		    , m_widget{m_window, std::forward<Args>(args)...}
+		{
+			m_window.resize(Size2d{800, 500}).show();
+		}
+
+	private:
+		Ui::Window m_window;
+		Widget m_widget;
+	};
+
+	class DocumentEditor
+	{
+		using FilterGraphEditorWindow = WidgetWithWithWindow<FilterGraphEditor>;
+
+	public:
+		DocumentEditor(): m_document{Size2d{512, 512}}
+		{
+			m_filter = std::make_unique<FilterGraphEditorWindow>("Texpainter", m_document);
+		}
 
 	private:
 		Model::Document m_document;
-		std::unique_ptr<FilterGraphEditor> m_filter;
+		std::unique_ptr<WidgetWithWithWindow<FilterGraphEditor>> m_filter;
 	};
 }
 
