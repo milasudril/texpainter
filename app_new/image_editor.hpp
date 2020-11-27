fedac4a537932c79a17014cbc801b0df82539d77
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-27 20:40:16 +0100

    Fill and expand. Add separators

--- app_new/image_editor.hpp
+++ app_new/image_editor.hpp
@@ -10,6 +10,7 @@
 #include "ui/combobox.hpp"
 #include "ui/palette_view.hpp"
 #include "ui/labeled_input.hpp"
+#include "ui/separator.hpp"
 
 #include <utility>
 
@@ -23,10 +24,13 @@ namespace Texpainter::App
 		    , m_root{owner, Ui::Box::Orientation::Vertical}
 		    , m_selectors{m_root, Ui::Box::Orientation::Horizontal}
 		    , m_image_sel{m_selectors, Ui::Box::Orientation::Horizontal, "Image: "}
+		    , m_sep_a{m_selectors}
 		    , m_brush_sel{m_selectors, Ui::Box::Orientation::Horizontal, "Brush: "}
+		    , m_sep_b{m_selectors}
 		    , m_pal_sel{m_selectors, Ui::Box::Orientation::Horizontal, "Palette: "}
-		    , m_palette{m_selectors}
-		    , m_img_view{m_root}
+		    , m_palette{m_selectors.insertMode(
+		          Ui::Box::InsertMode{0, Ui::Box::Fill | Ui::Box::Expand})}
+		    , m_img_view{m_root.insertMode(Ui::Box::InsertMode{0, Ui::Box::Fill | Ui::Box::Expand})}
 		{
 		}
 
@@ -35,7 +39,9 @@ namespace Texpainter::App
 		Ui::Box m_root;
 		Ui::Box m_selectors;
 		Ui::LabeledInput<Ui::Combobox> m_image_sel;
+		Ui::Separator m_sep_a;
 		Ui::LabeledInput<Ui::Combobox> m_brush_sel;
+		Ui::Separator m_sep_b;
 		Ui::LabeledInput<Ui::Combobox> m_pal_sel;
 		Ui::PaletteView m_palette;
 		Ui::ImageView m_img_view;
