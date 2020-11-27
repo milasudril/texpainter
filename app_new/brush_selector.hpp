0db757166eadc8d7a3c5d67266bc406fa508aa1a
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-27 21:25:11 +0100

    Add size slider to brush selector

--- app_new/brush_selector.hpp
+++ app_new/brush_selector.hpp
@@ -4,16 +4,26 @@
 #define TEXPAINTER_APP_BRUSHSELECTOR_HPP
 
 #include "ui/combobox.hpp"
+#include "ui/slider.hpp"
+#include "ui/box.hpp"
 
 namespace Texpainter::App
 {
 	class BrushSelector
 	{
 	public:
-		explicit BrushSelector(Ui::Container& owner): m_selector{owner} {}
+		explicit BrushSelector(Ui::Container& owner)
+		    : m_root{owner, Ui::Box::Orientation::Horizontal}
+		    , m_shape{m_root}
+		    , m_size{m_root.insertMode(Ui::Box::InsertMode{0, Ui::Box::Fill | Ui::Box::Expand}),
+		             false}
+		{
+		}
 
 	private:
-		Ui::Combobox m_selector;
+		Ui::Box m_root;
+		Ui::Combobox m_shape;
+		Ui::Slider m_size;
 	};
 }
 
