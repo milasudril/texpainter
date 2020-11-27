697df9f412a7a029e20c88d9b29ddcc25e450089
Torbjörn Rathsman <milasudril@gmail.com>
2020-11-27 20:33:59 +0100

    Add widgets to ImageEditor

--- app_new/image_editor.hpp
+++ app_new/image_editor.hpp
@@ -7,7 +7,11 @@
 #include "ui/box.hpp"
 #include "ui/image_view.hpp"
 #include "ui/palette_view.hpp"
-#include "ui/listbox.hpp"
+#include "ui/combobox.hpp"
+#include "ui/palette_view.hpp"
+#include "ui/labeled_input.hpp"
+
+#include <utility>
 
 namespace Texpainter::App
 {
@@ -18,6 +22,10 @@ namespace Texpainter::App
 		    : m_doc{doc}
 		    , m_root{owner, Ui::Box::Orientation::Vertical}
 		    , m_selectors{m_root, Ui::Box::Orientation::Horizontal}
+		    , m_image_sel{m_selectors, Ui::Box::Orientation::Horizontal, "Image: "}
+		    , m_brush_sel{m_selectors, Ui::Box::Orientation::Horizontal, "Brush: "}
+		    , m_pal_sel{m_selectors, Ui::Box::Orientation::Horizontal, "Palette: "}
+		    , m_palette{m_selectors}
 		    , m_img_view{m_root}
 		{
 		}
@@ -26,6 +34,10 @@ namespace Texpainter::App
 		std::reference_wrapper<Model::Document> m_doc;
 		Ui::Box m_root;
 		Ui::Box m_selectors;
+		Ui::LabeledInput<Ui::Combobox> m_image_sel;
+		Ui::LabeledInput<Ui::Combobox> m_brush_sel;
+		Ui::LabeledInput<Ui::Combobox> m_pal_sel;
+		Ui::PaletteView m_palette;
 		Ui::ImageView m_img_view;
 	};
 }
