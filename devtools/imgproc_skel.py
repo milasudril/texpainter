3333a2f24347cb33f8b6d3960bfe313a58c9973f
Torbjörn Rathsman <milasudril@gmail.com>
2020-12-05 16:14:52 +0100

    Initial work on DSL for creating imgprocs

---
+++ devtools/imgproc_skel.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import secrets
+import string
+import os
+
+template = string.Template("""//@	{
+//@	 "targets":[{"name":"$include_file", "type":"include"}]
+//@	}
+
+#ifndef $include_guard
+#define $include_guard
+
+#include "filtergraph/image_processor_id.hpp"
+#include "filtergraph/port_info.hpp"
+#include "filtergraph/img_proc_arg.hpp"
+#include "filtergraph/param_name.hpp"
+#include "filtergraph/param_value.hpp"
+$param_map_include
+$user_includes
+namespace $namespace_name
+{
+	using Texpainter::Size2d;
+	using Texpainter::FilterGraph::ImageProcessorId;
+	using Texpainter::FilterGraph::ImgProcArg;
+	using Texpainter::FilterGraph::ParamName;
+	using Texpainter::FilterGraph::ParamValue;
+	using Texpainter::FilterGraph::PortInfo;
+	using Texpainter::FilterGraph::PortType;
+	$imported_types
+	class ImageProcessor
+	{
+	public:
+		struct InterfaceDescriptor
+		{
+			$input_ports
+			$output_ports
+			$param_names
+		};
+
+
+		void operator()(ImgProcArg<InterfaceDescriptor> const& args) const
+		{
+			$call_operator
+		}
+
+		$param_accessors
+
+		static constexpr char const* name() { return "$processor_name"; }
+
+		static constexpr auto id() { return ImageProcessorId{"$processor_id"}; }
+
+	private:
+		$param_map
+		$user_state
+	};
+}
+
+#endif
+""")
+
+def makeIncludeFileName(name):
+	return name.replace(' ', '_').lower() + '.hpp'
+
+def makeNamespaceName(name):
+	parts = name.split(' ')
+	ret = []
+	for item in parts:
+		ret.append(item.capitalize())
+	return ''.join(ret)
+
+def splitall(path):
+	allparts = []
+	while 1:
+		parts = os.path.split(path)
+		if parts[0] == path:  # sentinel for absolute paths
+			allparts.insert(0, parts[0])
+			break
+		elif parts[1] == path: # sentinel for relative paths
+			allparts.insert(0, parts[1])
+			break
+		else:
+			path = parts[0]
+		allparts.insert(0, parts[1])
+	return allparts
+
+def makeIncludeGuard(filename):
+	cwd = os.path.split(os.getcwd())[1]
+	path = splitall(filename)
+	if path[0] == '.':
+		path = path[1:]
+	path = [cwd] + path
+
+	parts = []
+	for item in path:
+		res = item.replace('_', '')
+		res = res.replace('-', '')
+		res = res.replace('.', '_')
+		res = res.upper()
+		parts.append(res)
+
+	return '_'.join(parts)
+
+
+main_substitutes = dict()
+main_substitutes['processor_name'] = 'Foo Bar baz'
+main_substitutes['include_file'] = makeIncludeFileName(main_substitutes['processor_name'])
+main_substitutes['namespace_name'] = makeNamespaceName(main_substitutes['processor_name'])
+main_substitutes['include_guard'] = makeIncludeGuard(main_substitutes['include_file'])
+main_substitutes['param_map_include'] = ''
+main_substitutes['user_includes'] = ''
+main_substitutes['imported_types'] = ''
+main_substitutes['input_ports'] = 'static constexpr std::array<PortInfo, 0> InputPorts{};'
+main_substitutes['output_ports'] = 'static constexpr std::array<PortInfo, 0> OutputPorts{};'
+main_substitutes['param_names'] = 'static constexpr std::array<ParamName, 4> ParamNames{};'
+main_substitutes['call_operator'] = ''
+main_substitutes['param_accessors'] = ''
+main_substitutes['processor_id'] = secrets.token_hex(16)
+main_substitutes['param_map'] = ''
+main_substitutes['user_state'] = ''
+
+print(template.substitute(main_substitutes))
