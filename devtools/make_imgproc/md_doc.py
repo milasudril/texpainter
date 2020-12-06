20638b68fe607f74c9d7e9501a8a91919e838ac1
Torbjörn Rathsman <milasudril@gmail.com>
2020-12-06 14:05:27 +0100

    Draft for inline header

--- devtools/make_imgproc/md_doc.py
+++ devtools/make_imgproc/md_doc.py
@@ -1,34 +1,40 @@
+import re
+
 class Paragraph:
 	def __init__(self):
 		self.paragraphs = dict()  # Insertion order preserved as of Python 3.7
 		self.text = ''
 
-def headingLevel(md_line):
+def makeHeader(md_line, current_level):
+#	match = re.match(r'__[^:]*:__', md_line)
+#	if match:
+#		return (current_level + 1, md_line[2:md_line.find(':')], md_line[match.end():])
+
 	level = 0
 	for ch in md_line:
 		if ch == '#':
 			level = level + 1
 		else:
 			if level != 0 and ch == ' ':
-				return level
-			return 0
+				return (level, md_line[level:].strip())
+			return (level, md_line, md_line)
 
 def loadParagraphs(md_lines):
 	current_heading_level = 0
 	root = Paragraph()
 	current_paragraph = root
 	contexts = []
-	para_count = 0
 	is_code_block = False
 	for line in md_lines:
 		if line.startswith('```'):
 			is_code_block = not is_code_block
 		else:
-			heading_level = headingLevel(line)
+			res = makeHeader(line, current_heading_level)
+			heading_level = res[0]
 			if heading_level == 0:
-				current_paragraph.text += line
+				current_paragraph.text += res[2]
 			elif heading_level > current_heading_level:
-				header = line[heading_level + 1:].strip()
+				header = res[1] #line[heading_level + 1:].strip()
 				current_paragraph.paragraphs[header] = Paragraph()
 				contexts.append(current_paragraph)
 				current_paragraph = current_paragraph.paragraphs[header]
@@ -37,7 +43,7 @@ def loadParagraphs(md_lines):
 				for k in range(0,  current_heading_level - heading_level + 1):
 					current_paragraph = contexts.pop()
 
-				header = line[heading_level + 1:].strip()
+				header = header = res[1] # line[heading_level + 1:].strip()
 				current_paragraph.paragraphs[header] = Paragraph()
 				contexts.append(current_paragraph)
 				current_paragraph = current_paragraph.paragraphs[header]
