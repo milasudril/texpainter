a2e57c06a1e64045edb0ebb002bfd7ac269f6ed9
Torbjörn Rathsman <milasudril@gmail.com>
2020-12-06 17:04:16 +0100

    Take care of last line

--- devtools/make_imgproc/md_doc.py
+++ devtools/make_imgproc/md_doc.py
@@ -1,9 +1,11 @@
 import re
 
+
 class Paragraph:
 	def __init__(self):
 		self.paragraphs = dict()  # Insertion order preserved as of Python 3.7
 		self.text = []
+
 
 def makeHeader(md_line):
 	match = re.match(r'^__[^:]*:__', md_line)
@@ -18,6 +20,46 @@ def makeHeader(md_line):
 			if level != 0 and ch == ' ':
 				return (level, md_line[level:].strip())
 			return (level, '', md_line)
+
+
+def processLine(line, current_heading_level, current_paragraph, contexts, has_inline_header):
+	res = makeHeader(line)
+	if res != None:
+		heading_level = res[0]
+		if heading_level == 0:
+			if len(res[1]) != 0:
+				if has_inline_header:
+					current_paragraph = contexts.pop()
+				header = res[1]
+				current_paragraph.paragraphs[header] = Paragraph()
+				contexts.append(current_paragraph)
+				current_paragraph = current_paragraph.paragraphs[header]
+				has_inline_header = True
+			if len(res[2]) != 0:
+				current_paragraph.text.append(res[2])
+
+		elif heading_level > current_heading_level:
+			header = res[1]
+			current_paragraph.paragraphs[header] = Paragraph()
+			contexts.append(current_paragraph)
+			current_paragraph = current_paragraph.paragraphs[header]
+			current_heading_level = heading_level
+		else:
+			if has_inline_header:
+				has_inline_header = False
+				current_paragraph = contexts.pop()
+
+			for k in range(0, current_heading_level - heading_level + 1):
+				current_paragraph = contexts.pop()
+
+			header = header = res[1]
+			current_paragraph.paragraphs[header] = Paragraph()
+			contexts.append(current_paragraph)
+			current_paragraph = current_paragraph.paragraphs[header]
+			current_heading_level = heading_level
+
+	return (current_heading_level, current_paragraph, contexts, has_inline_header)
+
 
 def loadParagraphs(md_lines):
 	current_heading_level = 0
@@ -45,42 +87,10 @@ def loadParagraphs(md_lines):
 			else:
 				line = para_text.strip()
 				para_text = ''
-				res = makeHeader(line)
-				if res != None:
-					heading_level = res[0]
-					if heading_level == 0:
-						if len(res[1]) != 0:
-							if has_inline_header:
-								current_paragraph = contexts.pop()
-							header = res[1]
-							current_paragraph.paragraphs[header] = Paragraph()
-							contexts.append(current_paragraph)
-							current_paragraph = current_paragraph.paragraphs[header]
-							has_inline_header = True
-						if len(res[2]) != 0:
-							current_paragraph.text.append(res[2])
+				current_heading_level, current_paragraph, contexts, has_inline_header = processLine(
+					line, current_heading_level, current_paragraph, contexts, has_inline_header)
 
-					elif heading_level > current_heading_level:
-						header = res[1]
-						current_paragraph.paragraphs[header] = Paragraph()
-						contexts.append(current_paragraph)
-						current_paragraph = current_paragraph.paragraphs[header]
-						current_heading_level = heading_level
-					else:
-						if has_inline_header:
-							has_inline_header = False
-							current_paragraph = contexts.pop()
+	processLine(para_text.strip(), current_heading_level, current_paragraph, contexts,
+		has_inline_header)
 
-						for k in range(0,  current_heading_level - heading_level + 1):
-							current_paragraph = contexts.pop()
-
-						header = header = res[1]
-						current_paragraph.paragraphs[header] = Paragraph()
-						contexts.append(current_paragraph)
-						current_paragraph = current_paragraph.paragraphs[header]
-						current_heading_level = heading_level
-
-	print(para_text)
 	return root
-
-
