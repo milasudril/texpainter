d85f15117a2022f9422a5b41a0bb3577ed17224b
Torbjörn Rathsman <milasudril@gmail.com>
2020-12-06 13:21:50 +0100

    Check output from parser

--- devtools/test.py
+++ devtools/test.py
@@ -9,9 +9,10 @@ doc = make_imgproc.md_doc.loadParagraphs(lines)
 
 def dump(para, level):
 	for title, content in para.items():
-		for k in range(0, level):
-			print('	', end = '')
-		print(title)
+		for k in range(0, level + 1):
+			print('#', end = '')
+		print(title, end = '')
+		print(content.text, end = '')
 		dump(content.paragraphs, level + 1)
 dump(doc.paragraphs, 0)
 #print(doc.paragraphs)
