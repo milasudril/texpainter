33368c10a9cf3374ca688ff5ca78b307259b5eb4
Torbjörn Rathsman <milasudril@gmail.com>
2020-12-26 15:44:59 +0100

    Fix bugs in DiscretePdf

--- utils/discrete_pdf.hpp
+++ utils/discrete_pdf.hpp
@@ -15,25 +15,48 @@ namespace Texpainter
 	requires(N > 0) class DiscretePdf
 	{
 	public:
-		constexpr explicit DiscretePdf(std::array<ProbType, N> const& vals)
+		constexpr explicit DiscretePdf(std::array<ProbType, N> const& vals):m_pdf{}
 		{
-			std::partial_sum(std::begin(vals), std::end(vals), std::begin(m_pdf));
+			auto ge_zero = [](auto val){ return val > static_cast<ProbType>(0); };
+			auto i_begin = std::find_if(std::begin(vals), std::end(vals), ge_zero);
+			auto i_end = std::find_if(std::rbegin(vals), std::rend(vals), ge_zero).base();
+
+			if(i_begin == std::end(vals)) [[unlikely]]
+			{
+				std::ranges::fill(m_pdf, static_cast<ProbType>(1));
+				std::partial_sum(std::begin(m_pdf), std::end(m_pdf), std::begin(m_pdf));
+				std::transform(std::begin(m_pdf), std::end(m_pdf), std::begin(m_pdf), [](auto val){return val / N;});
+				m_first_nonzero = 0;
+				m_last_nonzero = N - 1;
+				return;
+			}
+			std::partial_sum(std::begin(vals), i_end, std::begin(m_pdf));
+
+			m_last_nonzero = i_end - std::begin(vals) - 1;
 			std::transform(std::begin(m_pdf),
 			               std::end(m_pdf),
 			               std::begin(m_pdf),
-			               [sum = m_pdf[N - 1]](auto val) { return val / sum; });
+			               [sum = m_pdf[m_last_nonzero]](auto val) {
+							   return val / sum;
+						});
+
+			m_first_nonzero = i_begin - std::begin(vals);
+			m_last_nonzero = i_end - std::begin(vals) - 1;
 		}
 
 		constexpr size_t eventIndex(ProbType random_value) const
 		{
-			auto i = std::ranges::upper_bound(m_pdf, random_value);
-			return std::min(N - 1, static_cast<size_t>(i - std::begin(m_pdf)));
+			auto const i = std::ranges::upper_bound(m_pdf, random_value);
+			auto const ret = static_cast<size_t>(i - std::begin(m_pdf));
+			return std::clamp(ret, m_first_nonzero, m_last_nonzero);
 		}
 
 		static constexpr size_t size() { return N; }
 
 	private:
 		std::array<ProbType, N> m_pdf;
+		size_t m_first_nonzero;
+		size_t m_last_nonzero;
 	};
 }
 
--- utils/discrete_pdf.test.cpp
+++ utils/discrete_pdf.test.cpp
@@ -9,6 +9,40 @@
 
 namespace Testcases
 {
+
+	void texpainterDiscretePdfOutOfBounds()
+	{
+		std::array<float, 7> weights{0.0f, 1.0f, 2.0f, 4.0f, 9.0f, 0.0f, 0.0f};
+
+		Texpainter::DiscretePdf pdf{weights};
+		assert(pdf.eventIndex(1.0f) == 4);
+		assert(pdf.eventIndex(std::nextafter(0.0f, -1.0f)) == 1);
+	}
+
+	void texpainterDiscretePdfAllZeros()
+	{
+		std::array<float, 4> weights{0.0f, 0.0f, 0.0f, 0.0f};
+
+		Texpainter::DiscretePdf pdf{weights};
+		std::random_device rng;
+		std::uniform_real_distribution U{0.0f, 1.0f};
+
+		std::array<size_t, 4> counts{};
+		constexpr auto N = 1024 * 1024 * 1024;
+		for(size_t k = 0; k < N; ++k)
+		{
+			++counts[pdf.eventIndex(U(rng))];
+		}
+
+		std::array<float, 4> output_probs{};
+		std::array<float, 4> expected_probs{0.25f, 0.25f, 0.25f, 0.25f};
+		std::ranges::transform(counts, std::begin(output_probs), [N](auto val) {
+			return static_cast<float>(std::round(16384.0 * val / N) / 16384.0);
+		});
+
+		assert(output_probs == expected_probs);
+	}
+
 	void texpainterDiscretePdfVerifyDistribution()
 	{
 		std::array<float, 4> weights{1.0f, 2.0f, 4.0f, 9.0f};
@@ -30,20 +64,15 @@ namespace Testcases
 		});
 		assert(weights == output_probs);
 	}
-
-	void texpainterDiscretePdfOutOfBounds()
-	{
-		std::array<float, 4> weights{1.0f, 2.0f, 4.0f, 9.0f};
-
-		Texpainter::DiscretePdf pdf{weights};
-		assert(pdf.eventIndex(1.0f) == weights.size() - 1);
-		assert(pdf.eventIndex(std::nextafter(0.0f, -1.0f)) == 0);
-	}
 }
 
-int main()
+int main(int argc, char*[])
 {
-	Testcases::texpainterDiscretePdfVerifyDistribution();
 	Testcases::texpainterDiscretePdfOutOfBounds();
+	if(argc > 2)
+	{
+		Testcases::texpainterDiscretePdfAllZeros();
+		Testcases::texpainterDiscretePdfVerifyDistribution();
+	}
 	return 0;
 }
