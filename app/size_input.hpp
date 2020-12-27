c0ff43ab712059afcb59715d462372ab999bcb94
Torbjörn Rathsman <milasudril@gmail.com>
2020-12-27 12:29:11 +0100

    Clamp size

--- app/size_input.hpp
+++ app/size_input.hpp
@@ -35,22 +35,22 @@ namespace Texpainter
 		    Ui::Slider::TickMark{Ui::logValue(std::numbers::phi, MinRatio, MaxRatio), "Φ:1"},
 		    Ui::Slider::TickMark{Ui::logValue(2.0, MinRatio, MaxRatio), "2:1"}};
 
-		static Size2d size_from_width(uint32_t width, double r)
+		static constexpr Size2d size_from_width(uint32_t width, double r)
 		{
 			auto const height = static_cast<uint32_t>(width / r + 0.5);
 			return Size2d{width, height};
 		}
 
-		static Size2d size_from_height(uint32_t height, double r)
+		static constexpr Size2d size_from_height(uint32_t height, double r)
 		{
 			auto const width = static_cast<uint32_t>(height * r + 0.5);
 			return Size2d{width, height};
 		}
 
-		static Size2d size_from_square_size(uint32_t size, double r)
+		static constexpr Size2d size_from_square_size(uint32_t size, double r)
 		{
 			// A = w*h
-			// w/h = r  <=> w = h*r
+			// w/h = r  <=> w = h*r <=> h = w/r
 			// A = s*s
 
 			// A = r*h*h
@@ -58,8 +58,8 @@ namespace Texpainter
 			// s = sqrt(r)*h
 			// h = s/sqrt(r)
 			// w = h*r = s*r/sqrt(r) = s*sqrt(r)
-			auto const w = static_cast<uint32_t>(size * sqrt(r) + 0.5);
-			auto const h = static_cast<uint32_t>(size / sqrt(r) + 0.5);
+			auto const w = static_cast<uint32_t>(size * std::sqrt(r) + 0.5);
+			auto const h = static_cast<uint32_t>(size / std::sqrt(r) + 0.5);
 			return Size2d{w, h};
 		}
 
@@ -67,6 +67,20 @@ namespace Texpainter
 
 		static constexpr SizeFunc s_size_mode[] = {
 		    size_from_width, size_from_height, size_from_square_size};
+
+		static constexpr uint32_t width_from_area(size_t A, double r)
+		{
+			return static_cast<uint32_t>(std::sqrt(r * A));
+		}
+
+		static constexpr uint32_t height_from_area(size_t A, double r)
+		{
+			return static_cast<uint32_t>(std::sqrt(A / r));
+		}
+
+		using LengthFunc = uint32_t (*)(size_t A, double r);
+
+		static constexpr LengthFunc s_size[] = {width_from_area, height_from_area, width_from_area};
 
 	public:
 		enum class ControlId : int
@@ -83,7 +97,6 @@ namespace Texpainter
 		    , m_size{m_root, Ui::Box::Orientation::Horizontal, "Width: "}
 		    , m_asplect_ratio{m_root, Ui::Box::Orientation::Vertical, "Aspect ratio:", false}
 		    , m_comp_size{m_root, Ui::Box::Orientation::Horizontal, "Size: ", "0"}
-		    , m_size_str{std::to_string(default_size.width())}
 		    , m_max_size{max_size}
 		{
 			m_size.label()
@@ -93,9 +106,9 @@ namespace Texpainter
 			    .eventHandler<ControlId::SizeMode>(*this);
 
 			m_size.inputField()
-			    .content(m_size_str.c_str())
 			    .alignment(0.0f)
 			    .width(5)
+			    .content(std::to_string(default_size.width()).c_str())
 			    .eventHandler<ControlId::SizeValue>(*this);
 
 			auto const r = static_cast<double>(default_size.width()) / default_size.height();
@@ -120,9 +133,19 @@ namespace Texpainter
 		template<auto, class T>
 		void onChanged(T&)
 		{
-			auto const size = value();
+			auto size = value();
+			if(size.area() > m_max_size.area()) [[unlikely]]
+				{
+					auto const size_mode = m_size.label().selected();
+					auto const r         = Ui::logValue(
+                        m_asplect_ratio.inputField().value(), MinRatio, MaxRatio, false);
+					auto const length = s_size[size_mode](m_max_size.area(), r);
+					m_size.inputField().content(std::to_string(length).c_str());
+					size = value();
+				}
+
 			m_comp_size.inputField().content(
-			    (toString(value()) + ", A = " + toString(size.area())).c_str());
+			    (toString(size) + ", A = " + toString(size.area())).c_str());
 		}
 
 		template<ControlId, class... T>
@@ -137,7 +160,6 @@ namespace Texpainter
 		Ui::LabeledInput<Ui::Slider> m_asplect_ratio;
 		Ui::LabeledInput<Ui::Label> m_comp_size;
 
-		std::string m_size_str;
 		Size2d m_max_size;
 
 		Ui::ErrorMessageDialog m_err_disp;
