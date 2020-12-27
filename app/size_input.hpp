8bfaef1efc342dd410610c86eef4760bbbd6d85d
Torbjörn Rathsman <milasudril@gmail.com>
2020-12-27 10:51:31 +0100

    Add tickmarks

--- app/size_input.hpp
+++ app/size_input.hpp
@@ -11,17 +11,35 @@
 #include "ui/slider.hpp"
 #include "utils/numconv.hpp"
 
+#include <numbers>
 #include <stdexcept>
 
 namespace Texpainter
 {
 	class SizeInput
 	{
+		static constexpr auto MinRatio = 0.5;
+		static constexpr auto MaxRatio = 2.0;
+
+		static constexpr Ui::Slider::TickMark RatioTickMarks[] = {
+		    Ui::Slider::TickMark{Ui::linValue(0.5, MinRatio, MaxRatio), "1:2"},
+		    Ui::Slider::TickMark{Ui::linValue(1.0 / std::numbers::phi, MinRatio, MaxRatio), "1:Φ"},
+		    Ui::Slider::TickMark{Ui::linValue(1.0 / std::numbers::sqrt2, MinRatio, MaxRatio),
+		                         "1:√2"},
+		    Ui::Slider::TickMark{Ui::linValue(std::numbers::sqrt3 / 2.0, MinRatio, MaxRatio),
+		                         "√3:2"},
+		    Ui::Slider::TickMark{Ui::linValue(1, MinRatio, MaxRatio), "1:1"},
+		    Ui::Slider::TickMark{Ui::linValue(2.0 / std::numbers::sqrt3, MinRatio, MaxRatio),
+		                         "2:√3"},
+		    Ui::Slider::TickMark{Ui::linValue(std::numbers::sqrt2, MinRatio, MaxRatio), "√2:1"},
+		    Ui::Slider::TickMark{Ui::linValue(std::numbers::phi, MinRatio, MaxRatio), "Φ:1"},
+		    Ui::Slider::TickMark{Ui::linValue(2.0, MinRatio, MaxRatio), "2:1"}};
+
 	public:
 		enum class ControlId : int
 		{
 			Width,
-			AspectRatio,
+			AspectRatio
 		};
 
 		explicit SizeInput(Ui::Container& container, Size2d default_size, Size2d max_size)
@@ -39,16 +57,20 @@ namespace Texpainter
 			    .width(5)
 			    .eventHandler<ControlId::Width>(*this);
 
+
 			auto const r = static_cast<double>(default_size.width()) / default_size.height();
 
-			m_asplect_ratio.inputField().value(Ui::linValue(1 / r, 0.5, 2.0));
+			m_asplect_ratio.inputField()
+			    .ticks(RatioTickMarks)
+			    .value(Ui::linValue(1 / r, MinRatio, MaxRatio));
 		}
 
 		Size2d value() const
 		{
 			auto const width  = static_cast<uint32_t>(std::atoi(m_size.inputField().content()));
 			auto const height = static_cast<uint32_t>(
-			    width / Ui::linValue(m_asplect_ratio.inputField().value(), 0.5, 2.0) + 0.5);
+			    width / Ui::linValue(m_asplect_ratio.inputField().value(), MinRatio, MaxRatio)
+			    + 0.5);
 
 			return Size2d{width, height};
 		}
