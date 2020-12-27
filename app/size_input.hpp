0c828cc8bc16032973ab81fb312f8733a1df5bd7
Torbjörn Rathsman <milasudril@gmail.com>
2020-12-27 10:56:29 +0100

    Switch to log scale

--- app/size_input.hpp
+++ app/size_input.hpp
@@ -18,22 +18,22 @@ namespace Texpainter
 {
 	class SizeInput
 	{
-		static constexpr auto MinRatio = 0.5;
-		static constexpr auto MaxRatio = 2.0;
+		static constexpr auto MinRatio = -1;
+		static constexpr auto MaxRatio = 1;
 
 		static constexpr Ui::Slider::TickMark RatioTickMarks[] = {
-		    Ui::Slider::TickMark{Ui::linValue(0.5, MinRatio, MaxRatio), "1:2"},
-		    Ui::Slider::TickMark{Ui::linValue(1.0 / std::numbers::phi, MinRatio, MaxRatio), "1:Φ"},
-		    Ui::Slider::TickMark{Ui::linValue(1.0 / std::numbers::sqrt2, MinRatio, MaxRatio),
+		    Ui::Slider::TickMark{Ui::logValue(0.5, MinRatio, MaxRatio), "1:2"},
+		    Ui::Slider::TickMark{Ui::logValue(1.0 / std::numbers::phi, MinRatio, MaxRatio), "1:Φ"},
+		    Ui::Slider::TickMark{Ui::logValue(1.0 / std::numbers::sqrt2, MinRatio, MaxRatio),
 		                         "1:√2"},
-		    Ui::Slider::TickMark{Ui::linValue(std::numbers::sqrt3 / 2.0, MinRatio, MaxRatio),
+		    Ui::Slider::TickMark{Ui::logValue(std::numbers::sqrt3 / 2.0, MinRatio, MaxRatio),
 		                         "√3:2"},
-		    Ui::Slider::TickMark{Ui::linValue(1, MinRatio, MaxRatio), "1:1"},
-		    Ui::Slider::TickMark{Ui::linValue(2.0 / std::numbers::sqrt3, MinRatio, MaxRatio),
+		    Ui::Slider::TickMark{Ui::logValue(1, MinRatio, MaxRatio), "1:1"},
+		    Ui::Slider::TickMark{Ui::logValue(2.0 / std::numbers::sqrt3, MinRatio, MaxRatio),
 		                         "2:√3"},
-		    Ui::Slider::TickMark{Ui::linValue(std::numbers::sqrt2, MinRatio, MaxRatio), "√2:1"},
-		    Ui::Slider::TickMark{Ui::linValue(std::numbers::phi, MinRatio, MaxRatio), "Φ:1"},
-		    Ui::Slider::TickMark{Ui::linValue(2.0, MinRatio, MaxRatio), "2:1"}};
+		    Ui::Slider::TickMark{Ui::logValue(std::numbers::sqrt2, MinRatio, MaxRatio), "√2:1"},
+		    Ui::Slider::TickMark{Ui::logValue(std::numbers::phi, MinRatio, MaxRatio), "Φ:1"},
+		    Ui::Slider::TickMark{Ui::logValue(2.0, MinRatio, MaxRatio), "2:1"}};
 
 	public:
 		enum class ControlId : int
@@ -45,7 +45,7 @@ namespace Texpainter
 		explicit SizeInput(Ui::Container& container, Size2d default_size, Size2d max_size)
 		    : m_root{container, Ui::Box::Orientation::Vertical}
 		    , m_size{m_root, Ui::Box::Orientation::Horizontal, "Width: "}
-		    , m_asplect_ratio{m_root, Ui::Box::Orientation::Horizontal, "Aspect ratio: ", false}
+		    , m_asplect_ratio{m_root, Ui::Box::Orientation::Horizontal, "W/H: ", false}
 		    , m_size_str{std::to_string(default_size.width())}
 		    , m_max_size{max_size}
 		{
@@ -62,14 +62,14 @@ namespace Texpainter
 
 			m_asplect_ratio.inputField()
 			    .ticks(RatioTickMarks)
-			    .value(Ui::linValue(1 / r, MinRatio, MaxRatio));
+			    .value(Ui::logValue(1 / r, MinRatio, MaxRatio));
 		}
 
 		Size2d value() const
 		{
 			auto const width  = static_cast<uint32_t>(std::atoi(m_size.inputField().content()));
 			auto const height = static_cast<uint32_t>(
-			    width / Ui::linValue(m_asplect_ratio.inputField().value(), MinRatio, MaxRatio)
+			    width / Ui::logValue(m_asplect_ratio.inputField().value(), MinRatio, MaxRatio)
 			    + 0.5);
 
 			return Size2d{width, height};
