518393aaf1779d685b92f51f0b7938bd898bc3c1
Torbjörn Rathsman <milasudril@gmail.com>
2020-12-27 10:09:25 +0100

    Replace label with combobox

--- app/size_input.hpp
+++ app/size_input.hpp
@@ -7,6 +7,7 @@
 #include "ui/labeled_input.hpp"
 #include "ui/text_entry.hpp"
 #include "ui/error_message_dialog.hpp"
+#include "ui/combobox.hpp"
 #include "utils/numconv.hpp"
 
 #include <stdexcept>
@@ -23,29 +24,34 @@ namespace Texpainter
 		};
 
 		explicit SizeInput(Ui::Container& container, Size2d default_size, Size2d max_size)
-		    : m_root{container, Ui::Box::Orientation::Horizontal}
-		    , m_width{m_root, Ui::Box::Orientation::Horizontal, "Width: "}
+		    : m_root{container, Ui::Box::Orientation::Vertical}
+		    , m_size{m_root, Ui::Box::Orientation::Horizontal, "Width: "}
 		    , m_height{m_root, Ui::Box::Orientation::Horizontal, "Height: "}
-		    , m_width_str{std::to_string(default_size.width())}
+		    , m_size_str{std::to_string(default_size.width())}
 		    , m_height_str{std::to_string(default_size.height())}
 		    , m_max_size{max_size}
 		{
-			m_width.inputField()
-			    .content(m_width_str.c_str())
-			    .alignment(1.0f)
+			m_size.label()
+				.append("Height: ")
+				.append("Square size: ")
+				.selected(0);
+
+			m_size.inputField()
+			    .content(m_size_str.c_str())
+			    .alignment(0.0f)
 			    .width(5)
 			    .eventHandler<ControlId::Width>(*this);
 
 			m_height.inputField()
 			    .content(m_height_str.c_str())
-			    .alignment(1.0f)
+			    .alignment(0.0f)
 			    .width(5)
 			    .eventHandler<ControlId::Height>(*this);
 		}
 
 		Size2d value() const
 		{
-			auto const width  = static_cast<uint32_t>(std::atoi(m_width.inputField().content()));
+			auto const width  = static_cast<uint32_t>(std::atoi(m_size.inputField().content()));
 			auto const height = static_cast<uint32_t>(std::atoi(m_height.inputField().content()));
 
 			return Size2d{width, height};
@@ -62,10 +68,10 @@ namespace Texpainter
 
 	private:
 		Ui::Box m_root;
-		Ui::LabeledInput<Ui::TextEntry> m_width;
+		Ui::LabeledInput<Ui::TextEntry, Ui::Combobox> m_size;
 		Ui::LabeledInput<Ui::TextEntry> m_height;
 
-		std::string m_width_str;
+		std::string m_size_str;
 		std::string m_height_str;
 		Size2d m_max_size;
 
@@ -78,7 +84,7 @@ namespace Texpainter
 		auto val = toInt(entry.content());
 		if(!val.has_value())
 		{
-			entry.content(m_width_str.c_str());
+			entry.content(m_size_str.c_str());
 			return;
 		}
 
