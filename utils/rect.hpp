934a325d86d602c4b82fc2828e87ea222ff90f15
Torbjörn Rathsman <milasudril@gmail.com>
2021-02-07 21:02:43 +0100

    Remove disabled code

--- utils/rect.hpp
+++ utils/rect.hpp
@@ -57,17 +57,6 @@ namespace Texpainter
 	inline vec2_t axisAlignedBoundingBox(vec2_t size, Angle ϴ)
 	{
 		return rotate(AxisAlignedBoundingBox{vec2_t{0.0, 0.0}, size}, ϴ).radius();
-#if 0
-		auto const corner       = size;
-		auto const rot_x        = vec2_t{cos(ϴ), -sin(ϴ)};
-		auto const rot_y        = vec2_t{sin(ϴ), cos(ϴ)};
-		auto const corner_a_rot = transform(corner, rot_x, rot_y);
-		// Mirror in x axis (wroks due to symmetry)
-		auto const corner_b_rot = transform(corner * vec2_t{1.0, -1.0}, rot_x, rot_y);
-
-		return vec2_t{std::max(std::abs(corner_a_rot[0]), std::abs(corner_b_rot[0])),
-		              std::max(std::abs(corner_a_rot[1]), std::abs(corner_b_rot[1]))};
-#endif
 	}
 
 	inline vec2_t axisAlignedBoundingBox(vec2_t size)
