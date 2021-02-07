e53281bd04cb499fb135f83b4d594f856fb33ba1
Torbjörn Rathsman <milasudril@gmail.com>
2021-02-07 20:49:34 +0100

    Create class for bounding box

--- utils/rect.hpp
+++ utils/rect.hpp
@@ -29,6 +29,52 @@ namespace Texpainter
 		return vec2_t{std::max(std::abs(corner_a[0]), std::abs(corner_b[0])),
 		              std::max(std::abs(corner_a[1]), std::abs(corner_b[1]))};
 	}
+
+	class AxisAlignedBoundingBox
+	{
+	public:
+		explicit AxisAlignedBoundingBox(vec2_t origin, vec2_t radius):
+		m_origin{origin}, m_radius{radius}
+		{
+		}
+
+		static AxisAlignedBoundingBox fromLimits(vec2_t lower, vec2_t upper)
+		{
+			return AxisAlignedBoundingBox{0.5*(upper + lower), 0.5*(upper - lower)};
+		}
+
+		vec2_t origin() const { return m_origin; }
+		vec2_t radius() const { return m_radius; }
+
+	private:
+		vec2_t m_origin;
+		vec2_t m_radius;
+	};
+
+	inline auto upperLimit(AxisAlignedBoundingBox box)
+	{
+		return box.origin() + box.radius();
+	}
+
+	inline auto lowerLimit(AxisAlignedBoundingBox box)
+	{
+		return box.origin() - box.radius();
+	}
+
+	inline auto size(AxisAlignedBoundingBox box)
+	{
+		return 2.0*box.radius();
+	}
+
+	inline AxisAlignedBoundingBox rotate(AxisAlignedBoundingBox box, Angle ϴ)
+	{
+		auto const rot_x = vec2_t{cos(ϴ), -sin(ϴ)};
+		auto const rot_y = vec2_t{sin(ϴ), cos(ϴ)};
+		auto const corner_a = abs(transform(box.radius(), rot_x, rot_y));
+		auto const corner_b = abs(transform(box.radius()*vec2_t{1.0, -1.0}, rot_x, rot_y));
+		auto const radius = max(corner_a, corner_b);
+		return AxisAlignedBoundingBox{box.origin(), radius};
+	}
 }
 
 #endif
