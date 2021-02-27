8b89f6e903bcdfe9cebf87e1d6a187822d968915
Torbjörn Rathsman <milasudril@gmail.com>
2021-02-27 09:24:11 +0100

    Add timestamp to changelog

--- changelog.py
+++ changelog.py
@@ -1,11 +1,13 @@
 import pygit2
+import datetime
 
 def collect_commits(repo):
 	ret = []
 	last = repo[repo.head.target]
 	for commit in repo.walk(last.id, pygit2.GIT_SORT_TIME):
 		shortmsg = commit.message.split('\n')[0]
-		ret.append([shortmsg, commit.short_id])
+		timestamp = datetime.datetime.fromtimestamp(commit.author.time)
+		ret.append([str(timestamp), shortmsg, commit.short_id])
 
 	return ret
 
