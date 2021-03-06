5ce24c911a1a2b9a538de8bc237214015aa7a653
Torbjörn Rathsman <milasudril@gmail.com>
2021-03-06 08:04:10 +0100

    Convert gamma

--- devtools/txp_render.sh
+++ devtools/txp_render.sh
@@ -7,4 +7,4 @@ target_dir=$3
 PATH=__targets_rel/cli:__targets_dbg/cli:__targets_gcov/cli:"$PATH"
 
 texporter --supersampling=8 "$src" "$target_dir"/temp.exr
-convert "$target_dir"/temp.exr $target
+convert "$target_dir"/temp.exr -colorspace sRGB $target
