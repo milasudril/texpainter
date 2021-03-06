de6e2914e0a08ae0bdde7d9a59a187e6214cd048
Torbjörn Rathsman <milasudril@gmail.com>
2021-03-06 07:59:25 +0100

    Add alternative paths to texporter

--- devtools/txp_render.sh
+++ devtools/txp_render.sh
@@ -4,5 +4,7 @@ src=$1
 target=$2
 target_dir=$3
 
-__targets_rel/cli/texporter --supersampling=8 "$src" "$target_dir"/temp.exr
+PATH=__targets_rel/cli:__targets_dbg/cli:__targets_gcov/cli:"$PATH"
+
+texporter --supersampling=8 "$src" "$target_dir"/temp.exr
 convert "$target_dir"/temp.exr $target
