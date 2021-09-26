d63b6e51ed96c29c70a9f5986a857046605541db
Torbjörn Rathsman <milasudril@gmail.com>
2021-09-26 12:45:07 +0200

    Add up-to-date check

--- filtergraph/node_output.hpp
+++ filtergraph/node_output.hpp
@@ -9,8 +9,6 @@
 
 #include "utils/inplace_mutator.hpp"
 
-#include <map>
-
 namespace Texpainter::FilterGraph
 {
 	class NodeOutput
@@ -21,7 +19,9 @@ namespace Texpainter::FilterGraph
 
 		Node::result_type const& result() const { return m_result; }
 
-		bool isUpToDate() const { return m_last_modified <= m_last_rendered; }
+		size_t lastModified() const { return m_last_modified; }
+
+		size_t lastRendered() const { return m_last_rendered; }
 
 		auto inputs() const { return m_source.get().inputs(); }
 
@@ -35,12 +35,49 @@ namespace Texpainter::FilterGraph
 			return true;
 		}
 
+		Node const& source() const
+		{
+			return m_source.get();
+		}
+
 	private:
 		size_t m_last_modified;
 		size_t m_last_rendered;
 		std::reference_wrapper<Node> m_source;
 		Node::result_type m_result;
 	};
+
+	size_t lastUpdated(NodeOutput const& result)
+	{
+		return std::max(result.lastModified(), result.lastRendered());
+	}
+
+	template<class NodeOutputMap>
+	size_t lastUpdated(NodeOutput const& result, NodeOutputMap const& result_map)
+	{
+		auto last_updated = lastUpdated(result);
+		auto inputs = result.source().inputs();
+		for(size_t k = 0; k != std::size(inputs); ++k)
+		{
+			if(!inputs[k].valid()) [[unlikely]]
+			{ continue; }
+
+			auto& src = inputs[k].processor();
+			auto i = result_map.find(inputs[i].nodeId());
+			if(i == std::end(result_map)) [[unlikely]]
+			{ continue; }
+
+			last_updated = std::max(lastUpdated(i->second, result_map), last_updated);
+		}
+
+		return last_updated;
+	}
+
+	template<class NodeOutputMap>
+	bool isUpToDate(NodeOutput const& result, NodeOutputMap const& result_map)
+	{
+		return result.lastRendered() >= lastUpdated(result, result_map);
+	}
 }
 
 #endif
