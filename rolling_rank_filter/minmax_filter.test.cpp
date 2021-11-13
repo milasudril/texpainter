40a6c4033fbbbcfc0f806ff726f41062bfd438c8
Torbjörn Rathsman <milasudril@gmail.com>
2021-11-13 16:32:01 +0100

    Verify that mask moves properly

--- rolling_rank_filter/minmax_filter.test.cpp
+++ rolling_rank_filter/minmax_filter.test.cpp
@@ -6,6 +6,7 @@
 
 #include <cassert>
 #include <numeric>
+#include <set>
 
 namespace Testcases
 {
@@ -22,8 +23,10 @@ namespace Testcases
 		 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		// clang-format on
 
+		 auto const mask_span =Texpainter::Span2d<int8_t const>{std::data(mask), 9, 7};
+
 		auto line_deltas = Texpainter::RollingRankFilter::genLineDelta(
-		    Texpainter::Span2d<int8_t const>{std::data(mask), 9, 7});
+		    mask_span);
 
 		assert(std::size(line_deltas) == 12);
 		assert((std::ranges::max_element(line_deltas, [](auto a, auto b) { return a.x < b.x; })->x
@@ -47,17 +50,50 @@ namespace Testcases
 		                                 [](int32_t a, auto b) { return a + b.sign; });
 		assert(sum == 0);
 
+		std::multiset<std::pair<uint32_t, uint32_t>> active_set;
+		for(uint32_t y = 0; y != mask_span.height() - 1; ++y)
+		{
+			for(uint32_t x = 0; x != mask_span.width() - 1; ++x)
+			{
+				if(mask_span(x, y) != 0)
+				{ active_set.insert(std::pair{x, y}); }
+			}
+		}
+		assert(std::size(active_set) == 36);
+
+		std::ranges::for_each(line_deltas, [&active_set](auto item){
+			if(item.sign > 0)
+			{
+				active_set.insert(std::pair{item.x, item.y});
+			}
+			else
+			{
+				auto i = active_set.find(std::pair{item.x, item.y});
+				assert(i != std::end(active_set));
+				active_set.erase(i);
+			}
+		});
+		assert(std::size(active_set) == 36);
+
 		// clang-format off
-		constexpr std::array<int8_t, 54> mask_res{
-		  0,  0, -1, 0, 0, 0, 1, 0, 0,
-		  0, -1,  0, 0, 0, 0, 0, 1, 0,
-		 -1,  0,  0, 0, 0, 0, 0, 0, 1,
-		 -1,  0,  0, 0, 0, 0, 0, 0, 1,
-		  0, -1,  0, 0, 0, 0, 0, 1, 0,
-		  0,  0, -1, 0, 0, 0, 1, 0, 0};
+		constexpr std::array<int8_t, 63> expected_mask_after_move{
+		  0, 0, 0, 1, 1, 1, 1, 0, 0,
+		  0, 0, 1, 1, 1, 1, 1, 1, 0,
+		  0, 1, 1, 1, 1, 1, 1, 1, 1,
+		  0, 1, 1, 1, 1, 1, 1, 1, 1,
+		  0, 0, 1, 1, 1, 1, 1, 1, 0,
+		  0, 0, 0, 1, 1, 1, 1, 0, 0,
+		  0, 0, 0, 0, 0, 0, 0, 0, 0
+		};
 		// clang-format on
 
-		assert(std::ranges::equal(mask_res, delta_mask.pixels()));
+		Texpainter::PixelStore::Image<int8_t> mask_after_move{9, 7};
+		std::ranges::fill(mask_after_move.pixels(), 0);
+		std::ranges::for_each(active_set, [&mask_after_move](auto val) {
+			mask_after_move(val.first, val.second) = 1;
+		});
+
+		assert(std::ranges::equal(expected_mask_after_move, mask_after_move.pixels()));
 	}
 
 	void texpainterRollingRankFilterGenLineDelta1By1()
