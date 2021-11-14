5e0db26c3b5f51fc4e4a4fad8a71ed265d20a6a8
Torbjörn Rathsman <milasudril@gmail.com>
2021-11-14 10:32:59 +0100

    Add wrapper to minmaxFilter

--- rolling_rank_filter/minmax_filter.hpp
+++ rolling_rank_filter/minmax_filter.hpp
@@ -6,6 +6,8 @@
 #ifndef TEXPAINTER_ROLLINGRANKFILTER_MINMAXFILTER_HPP
 #define TEXPAINTER_ROLLINGRANKFILTER_MINMAXFILTER_HPP
 
+#include "./utils.hpp"
+
 #include "utils/span_2d.hpp"
 
 #include <cstdint>
@@ -13,6 +15,14 @@
 namespace Texpainter::RollingRankFilter
 {
 	void minmaxFilter(Span2d<float const> src, Span2d<int8_t const> mask, float* min, float* max);
+
+	inline void minmaxFilter(Span2d<float const> src, float const* mask, float* min, float* max)
+	{
+		auto const binary_mask = quantize(Span2d{mask, src.width(), src.height()});
+		auto const bb = boundingBox(binary_mask.pixels(), [](auto val) { return val > 0; });
+		auto const mask_cropped = PixelStore::crop(binary_mask.pixels(), bb, 1, 1);
+		minmaxFilter(src, mask_cropped.pixels(), min, max);
+	}
 }
 
 #endif
