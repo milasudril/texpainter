afabec84f0b91c4cb94a8e492fd12c5c4e9dc2b3
Torbjörn Rathsman <milasudril@gmail.com>
2021-11-23 22:19:44 +0100

    Reenable txp render

--- devtools/txp_render.sh
+++ devtools/txp_render.sh
@@ -6,5 +6,5 @@ target_dir=$3
 
 PATH=__targets_rel/cli:__targets_dbg/cli:__targets_gcov/cli:"$PATH"
 
-#texporter --supersampling=8 "$src" "$target_dir"/temp.exr
-#convert "$target_dir"/temp.exr -colorspace sRGB $target
+texporter --supersampling=8 "$src" "$target_dir"/temp.exr
+convert "$target_dir"/temp.exr -colorspace sRGB $target
